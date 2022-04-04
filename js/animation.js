30647d9037316256b51cac4a085e3a446fc491b7
abhinav-18max <abhinav200218@gmail.com>
2022-04-04 12:53:10 +0530

    set

---
+++ js/animation.js
@@ -0,0 +1,8 @@
+//<script src="./anime.min.js"></script>
+const container = document.querySelector(".hero");
+for(var i=0;i<=100;i++){
+    const blocks =document.createElement('div');
+    blocks.classList.add('block');
+    container.appendChild(blocks)
+
+}
